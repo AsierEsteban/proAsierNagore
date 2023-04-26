7cdb452fbbb2fbc71e943c76ff817b468d5e3221
hpober <srdefhd@gmail.com>
2023-04-26 22:19:25 +0200

    Commit conflicto

--- src/Prueba/Prueba1.java
+++ src/Prueba/Prueba1.java
@@ -8,6 +8,7 @@ public class Prueba1 {
 		System.out.println("Tercero");
 		System.out.println("Cambio de Nagore");
 		System.out.println("Cambio de asier");
+		System.out.println("Cambio de asier segundo");
 	}
 	
 }
