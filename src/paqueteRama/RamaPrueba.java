ab486f4f38b40a2c63dfca7bcbf476dbdfc84795
hpober <srdefhd@gmail.com>
2023-04-26 22:54:38 +0200

    Siguente cambio en rama

--- src/paqueteRama/RamaPrueba.java
+++ src/paqueteRama/RamaPrueba.java
@@ -5,6 +5,7 @@ public class RamaPrueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Cambio de asier!");
+		System.out.println("Cambio en rama");
 	}
 
 }
